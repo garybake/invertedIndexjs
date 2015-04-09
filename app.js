9e696c361fd56d27b888825dc9ce1d8da72b35f7
garybake <garybake@gmail.com>
2015-04-09 13:05:29 +0100

    moved indexing function to module

--- app.js
+++ app.js
@@ -1,61 +1,18 @@
-// http://en.wikipedia.org/wiki/Inverted_index
 
-var invertedIndex = {
-    docs: [],
-    idx: {}
-};
 
-var d = [];
-d[0] = "it is what it is";
-d[1] = "what is it";
-d[2] = "it is a banana";
+var InvIndex = require('./lib/invertedIndex');
+var invIndex = new InvIndex();
 
-function addDoc(doc){
-    var invIdx = invertedIndex.idx;
-    invertedIndex.docs.push(doc);
-    var docId = invertedIndex.docs.length - 1;
-    var words = doc.split(" ");
-    words.forEach(function(word, widx){
-        if (invIdx.hasOwnProperty(word)) {
-            var arr = invIdx[word];
-            if (arr.indexOf(docId) === -1){
-                arr.push(docId);
-            }
-        } else {
-            invIdx[word] = [docId];
-        }
-    });
-}
-
-function searchWord(word){
-    return(invertedIndex.idx[word] ? invertedIndex.idx[word] : []);
-}
-
-function searchQuery(words){
-    var queryWords = words.split(" ");
-    var results = searchWord(queryWords[0]);
-    for (var i = 1; i < queryWords.length; i++){
-        var matches = searchWord(queryWords[i]);
-        results = mergeArray(results, matches);
-    }
-    return results;
-}
-
-function mergeArray(a, b){
-    var c = a.concat(b.filter(function (item) {
-        return a.indexOf(item) < 0;
-    }));
-    return c;
-}
-
-// -----------------------
+var d = [ 
+    "it is what it is",
+    "what is it",
+    "it is a banana",
+    "this is a new banana"
+];
 
 d.forEach(function(doc){
-    addDoc(doc);
+    invIndex.addDoc(doc);
 });
 
-addDoc("this is a new banana");
-
-console.log(invertedIndex);
-
-console.log(searchQuery('what new'));
+console.log(invIndex.data);
+console.log(invIndex.searchQuery('what new'));
